ca7ab0a2b6d385c759d73b5c689122ef09c9c89b
Bai Li - NOAA <59936250+Bai-Li-NOAA@users.noreply.github.com>
2025-06-24 16:28:32 +0000

    add pkg name before calling functions

--- data-raw/ewe_nwatlantic.R
+++ data-raw/ewe_nwatlantic.R
@@ -2,18 +2,18 @@
 
 # TODO: remove this line after testing the update-data-r GHA; run install()
 # The functional groups included in the model
-functional_groups <- get_functional_groups(
+functional_groups <- ecosystemdata::get_functional_groups(
   file_path = fs::path(
     "data-raw", "ewe_nwatlantic", "base_run", "basic_estimates.csv"
   )
 )
 
-ewe_nwatlantic_base <- load_model(
+ewe_nwatlantic_base <- ecosystemdata::load_model(
   directory = fs::path("data-raw", "ewe_nwatlantic", "base_run"),
   type = "ewe",
   functional_groups = functional_groups
 )
-ewe_nwatlantic_env <- load_model(
+ewe_nwatlantic_env <- ecosystemdata::load_model(
   directory = fs::path("data-raw", "ewe_nwatlantic", "environmental_link"),
   type = "ewe",
   functional_groups = functional_groups
